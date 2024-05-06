61c0d02362c4c1fbce7d249b572705107c5a6b16
progradan <169004133+progradan@users.noreply.github.com>
2024-05-06 17:15:25 -0400

    Add files via upload

---
+++ listatares.py
@@ -0,0 +1,9 @@
+a= "HOLAMUNDO, MAÑANA, TARDE, NOCHE, 1234"
+lista= a.split()
+print(a)
+print("frase = " +lista[0])
+print(" Día = " +lista[1])
+print("lugar = " +lista[2])
+print("cena = " +lista[3])
+print("números = " +lista[4])
+
